6159a43af44648beb518c68fc3061a5989845792
Ayushman Bhattacharya <74301576+Circuit-Overtime@users.noreply.github.com>
2023-12-26 23:57:54 +0530

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -359,7 +359,7 @@ window.onblur = function () {
     link.rel = 'icon';
     document.head.appendChild(link);
   }
-  link.href = './IMG/crying_clock.png';
+  link.href = '/IMG/crying_clock.png';
 }
 
 window.onfocus = function () {
@@ -371,6 +371,6 @@ window.onfocus = function () {
     link.rel = 'icon';
     document.head.appendChild(link);
   }
-  link.href = './IMG/clock_ico.png';
+  link.href = '/IMG/clock_ico.png';
 
 }
